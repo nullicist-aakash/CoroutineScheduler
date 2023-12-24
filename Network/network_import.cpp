70a2a7c898b3748092793f569b00fbc47be08f91
madhavgupta2775 <guptamadhav2775@gmail.com>
2023-12-24 20:17:00 +0530

    Fixed windows error message

--- Network/network_import.cpp
+++ Network/network_import.cpp
@@ -5,8 +5,14 @@ using namespace std;
 string get_err_str()
 {
 #ifdef WINDOWS
-    DWORD error = ::GetLastError();
-    return system_category().message(error);
+    LPSTR messageBuffer = nullptr;
+    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, ::GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+    std::string message(messageBuffer, size);
+    LocalFree(messageBuffer);
+
+    return message;
 #elif defined(UNIX)
     return strerror(errno);
 #endif
