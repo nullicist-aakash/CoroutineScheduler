596f38e2a4bdb3b487446519ae1d74a056e64e7f
madhavgupta2775 <guptamadhav2775@gmail.com>
2024-01-11 14:33:57 +0530

    implemented add_destination and remove_destination, also added bigint source code.

--- ring/ring.cpp
+++ ring/ring.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <map>
 #include "SHA256.h"
 #include "bigint.h"
 
@@ -19,15 +20,29 @@ private:
         return bigint(hash);
     }
     set<string> destinations;
+    map<string, vector<bigint>> node_to_vnode; // node, vnodes
+    map<bigint, string> vnode_to_node; // vnode, node
 public:
     void add_destination(string IP) {
         destinations.insert(IP);
-        // other logic
+        vector<bigint> vnodes;
+        for (int i = 0; i < replication_count; i++) {
+            bigint vnode = hash(IP + to_string(i));
+            if (vnode_to_node.find(vnode) == vnode_to_node.end()) {
+                vnodes.push_back(vnode);
+                vnode_to_node[vnode] = IP;
+            }
+        }
+        node_to_vnode[IP] = vnodes;
 
     }
     void remove_destination(string IP) {
         destinations.erase(IP);
-        // other logic
+        vector<bigint> vnodes = node_to_vnode[IP];
+        for (bigint vnode : vnodes) {
+            vnode_to_node.erase(vnode);
+        }
+        node_to_vnode.erase(IP);
     }
     string find_destination(string input) {
         int hash_input = hash(input);
